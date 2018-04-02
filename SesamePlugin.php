dde96b9c179dd9e2123ef19d39b6c4f922ce6b3b
Nils Abegg <nils.abegg@gmail.com>
2018-04-02 23:55:42 +0200

    Changed Subscriber to make ExtJS Extension work

--- SesamePlugin.php
+++ SesamePlugin.php
@@ -21,30 +21,4 @@ class SesamePlugin extends Plugin
         $container->setParameter('sesame_plugin.plugin_dir', $this->getPath());
         parent::build($container);
     }
-    /**
-     * {@inheritdoc}
-     */
-    public function install(InstallContext $context)
-    {
-        /*
-         * The following code sets the initial value of the sendMail configuration in the plugin to the core config value of "sendOrderMail".
-         * If the plugin has been configured already, it will not overwrite the existing value.
-         */
-        /** @var array $pluginConfig */
-        $pluginConfig = $this->container->get('shopware.plugin.cached_config_reader')->getByPluginName($this->getName());
-        //$sendMailConfigGlobal = (bool) $this->container->get('config')->get('sendOrderMail');
-        /*
-         * If there is a plugin configuration already, or the core value equals false anyway, it's not required to set
-         * the initial config value again. Therefore it returns before executing the next part.
-         */
-        //if (!$sendMailConfigGlobal || isset($pluginConfig['sendMail'])) {
-            //return;
-        //}
-        /** @var InstallerService $pluginManager */
-        $pluginManager = $this->container->get('shopware_plugininstaller.plugin_manager');
-        /** @var PluginModel $plugin */
-        $plugin = $pluginManager->getPluginByName($this->getName());
-        //Finally set the plugin config value to the core config value.
-        // $pluginManager->saveConfigElement($plugin, 'sendMail', $sendMailConfigGlobal);
-    }
 }
